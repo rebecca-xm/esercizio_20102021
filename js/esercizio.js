5769ae86e6149beb57cb50c9e729b130cebf9b15
Rebecca Mollica <rebecca.mollica@live.it>
2021-10-20 16:45:41 +0200

    esercizio js

---
+++ js/esercizio.js
@@ -0,0 +1,21 @@
+/* CONSEGNA
+[
+  [“Giacomo”, “Stefania”, “Sandro”, “Maria”, “Marta”, “Giorgia”, “Franco”, Valentina],
+  [12, 32, 21, 47, 6, 78, 54, 32]
+] */ 
+
+let str = prompt("Inserisci 5 nomi separati da una virgola", `Stefania,Sandro,Maria,Giacomo,Giorgia`);
+let arrAge = [32, 21, 47, 12, 78];
+
+let arr1 = str.split([,]);
+
+arr1.push('Franco', 'Valentina');
+arr1.unshift('Giacomo');
+arr1.splice(4,1,'Marta');
+
+arrAge.push(54, 32);
+arrAge.unshift(12);
+arrAge.splice(4,1,6);
+
+let test = [arr1, arrAge];
+console.log(test);
